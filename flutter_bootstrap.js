ef45677bd7ffeff3573ffa6874c735d86e162704
이강훈 <chinggu88@gmail.com>
2024-12-01 21:00:26 +0900

    web deploy setting...

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2007977211"
+    serviceWorkerVersion: "3107862622"
   }
 });
