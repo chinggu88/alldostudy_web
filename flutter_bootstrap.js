083226b6fa499a65bbce4a524182288645f254f0
이강훈 <chinggu88@gmail.com>
2024-12-01 21:43:13 +0900

    로딩바추가..

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1330467167"
+    serviceWorkerVersion: "3719697407"
   }
 });
